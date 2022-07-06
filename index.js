01b4b0aa4c2ee96a3e5631412fbd856d03492b40
Isai <castanedaisai303@gmail.com>
2022-07-06 09:01:03 -0700

    commit tree 7aea47774c99c9f6d0c6d70645e698268cccd13a

---
+++ index.js
@@ -0,0 +1,18 @@
+function whisper(string) {
+    return string.toLowerCase();
+  }
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  function logShout(string) {
+    console.log(string.toUpperCase());
+ }
+ function sayHiToHeadphonedRoommate(string) {
+
+    return string.toUpperCase();
+    return string.toUpperCase();
+    console.log(string === string.toLowerCase());
+ }
